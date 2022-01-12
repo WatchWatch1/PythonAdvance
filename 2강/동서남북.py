1ca8a901940e2505ee8f27c37a6f31faff42edd2
WatchWatch1 <91450685+WatchWatch1@users.noreply.github.com>
2022-01-12 14:49:22 +0900

    Create 동서남북.py

---
+++ 2강/동서남북.py
@@ -0,0 +1,34 @@
+from turtle import *
+
+t1 = Turtle()
+t1.shape("turtle")
+t1.color("purple")
+t1.seth(90)
+for i in range(20):
+
+    t1.forward(100)
+    t1.right(90)
+    t1.forward(100)
+    t1.right(90)
+    t1.forward(100)
+    t1.right(90)
+    t1.forward(100)
+    t1.right(72)
+
+t3 = Turtle()
+t3.shape("turtle")
+t3.color("red")
+t3.seth(270)
+for i in range(6):
+    t3.forward(100)
+    t3.right(90)
+    t3.forward(100)
+    t3.right(90)
+    t3.forward(100)
+    t3.right(90)
+    t3.forward(100)
+    t3.right(36)
+
+
+
+done()
